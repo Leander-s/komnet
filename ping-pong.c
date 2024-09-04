d7563e911fc3f020b5e5120f40d867a916179fa9
Leander <leander.funken@live.de>
2024-09-04 13:00:38 +0200

    Random messages and message size in args

--- ping-pong.c
+++ ping-pong.c
@@ -1,63 +1,102 @@
+#include <errno.h>
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-int root_func(int size) {
-  char buffer[32];
-  memset(buffer, 0, 32);
+int string_to_int(char *str) {
+  errno = 0;
+  char *end;
+  const long i = strtol(str, &end, 10);
+  const int range_error = errno == ERANGE;
+  return (int)(i);
+}
+
+void generate_random_message(char *buffer, int bufferSize, int seed) {
+  // init rng
+  srandom(seed);
+
+  for (int i = 0; i < bufferSize; i++) {
+    buffer[i] = random() % (91 - 33) + 33;
+  }
+}
+
+int root_func(int size, int messageSize) {
+  // technically sizof(char) = 1 but this gets the point across better
+  char *buffer = (char *)malloc(sizeof(char) * messageSize);
   int err;
-  sprintf(buffer, "Hello from root");
+
+  memset(buffer, 0, messageSize);
+  generate_random_message(buffer, messageSize, time(NULL));
   for (int i = 1; i < size; i++) {
-    err = MPI_Send((void*)buffer, 32, MPI_CHAR, i, 1, MPI_COMM_WORLD);
-    if(err != MPI_SUCCESS){
-        printf("Send error in root\n");
-        return err;
+    err = MPI_Send((void *)buffer, messageSize, MPI_CHAR, i, 1, MPI_COMM_WORLD);
+    if (err != MPI_SUCCESS) {
+      printf("Send error in root\n");
+      free(buffer);
+      return err;
     }
   }
-  memset(buffer, 0, 32);
-  for(int i = 1; i < size; i++){
-      err = MPI_Recv((void*)buffer, 32, MPI_CHAR, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      if(err != MPI_SUCCESS){
-          printf("Read error in root\n");
-          return err;
-      }
+  memset(buffer, 0, messageSize);
+  for (int i = 1; i < size; i++) {
+    err = MPI_Recv((void *)buffer, messageSize, MPI_CHAR, i, 1, MPI_COMM_WORLD,
+                   MPI_STATUS_IGNORE);
+    if (err != MPI_SUCCESS) {
+      printf("Read error in root\n");
+      free(buffer);
+      return err;
+    }
 
-      // Print what was read from node i.
-      printf("Root: Read '%s' from node %d.\n", (char*)buffer, i);
+    // Print what was read from node i.
+    printf("Root: Received from node %d : '%s'\n", i, (char *)buffer);
   }
+  free(buffer);
   return MPI_SUCCESS;
 }
 
-int other_func(int rank) {
-  char buffer[32];
-  memset(buffer, 0, 32);
+int other_func(int rank, int messageSize) {
+  char *buffer = (char *)malloc(sizeof(char) * messageSize);
   int err;
-  err = MPI_Recv((void *)buffer, 32, MPI_CHAR, 0, 1, MPI_COMM_WORLD,
+  memset(buffer, 0, messageSize);
+  err = MPI_Recv((void *)buffer, messageSize, MPI_CHAR, 0, 1, MPI_COMM_WORLD,
                  MPI_STATUS_IGNORE);
   if (err != MPI_SUCCESS) {
     printf("Read error in %d\n", rank);
+    free(buffer);
     return err;
   }
 
   // print what was read from root
-  printf("Node %d: Read '%s' from root.\n", rank, (char*)buffer);
+  printf("Node %d: Received from root : '%s'\n", rank, (char *)buffer);
 
-  memset(buffer, 0, 32);
-  sprintf(buffer, "Hello from %d", rank);
-  err = MPI_Send((void *)buffer, 32, MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+  memset(buffer, 0, messageSize);
+  generate_random_message(buffer, messageSize, time(NULL)*rank);
+  err = MPI_Send((void *)buffer, messageSize, MPI_CHAR, 0, 1, MPI_COMM_WORLD);
   if (err != MPI_SUCCESS) {
     printf("Send error in %d\n", rank);
+    free(buffer);
     return err;
   }
+  free(buffer);
   return MPI_SUCCESS;
 }
 
 int main(int argc, char **argv) {
-  int err, rank, size;
+  int err, rank, size, messageSize = 16;
   err = MPI_Init(&argc, &argv);
   if (err != 0) {
     printf("Error initializing MPI!\n");
     return 1;
+  }
+
+  for (int i = 0; i < argc; i++) {
+    if (strcmp(argv[i], "-s") == 0) {
+      if (i + 1 >= argc) {
+        printf("Specify message size '-s <size>'. Using 16 by default.\n");
+        continue;
+      }
+      messageSize = string_to_int(argv[i + 1]);
+    }
   }
 
   err = MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -70,11 +109,10 @@ int main(int argc, char **argv) {
     printf("Error getting rank of process!\n");
     return 1;
   }
-  printf("Rank : %d of %d\n", rank, size);
-  if(rank == 0){
-      root_func(size);
-  }else{
-      other_func(rank);
+  if (rank == 0) {
+    root_func(size, messageSize);
+  } else {
+    other_func(rank, messageSize);
   }
   MPI_Finalize();
   return 0;
