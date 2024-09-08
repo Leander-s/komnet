caa5f840c10192f094046565a719380fb28ed15a
Leander <lfunken@students.uni-mainz.de>
2024-09-08 12:25:57 +0200

    run scripts, results

--- run_ping_exchange.sh
+++ run_ping_exchange.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-#SBATCH --job-name=ping_exchange
-#SBATCH --partition=largemem
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2
-#SBATCH --time=00:01:00
-#SBATCH --output=./ping_exchange_out/pe.out
-#SBATCH --error=./ping_exchange_err/pe.err
-
-sbatch mpirun -np 2 -mca btl tcp,self ./build/ping_exchange
+export OMPI_MCA_btl=tcp,self
+srun --partition largemem -N 2 -n 2 ./build/ping_exchange
---
+++ run_ping_pong.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export OMPI_MCA_btl=tcp,self
+srun --partition largemem -N 2 -n 2 ./build/pingpong
