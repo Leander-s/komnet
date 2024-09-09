71f55d71df94f145182773a9bfaec0dc9ccd6d34
Funken, Leander Mathias Heinrich <lfunken@students.uni-mainz.de>
2024-09-09 12:03:59 +0200

    Reapply "multiple cycles in measurements"
    This reverts commit 1f8aeda60ac5b7d0874bcd5746076a935a6e4cd1.

--- src/ping-exchange.c
+++ src/ping-exchange.c
@@ -2,52 +2,64 @@
 #include <mpi.h>
 
 int ping_exchange_root_run(int size, int messageSize, int verbose) {
+  double latencySum = 0;
+  int err, cycles = 100;
   char sendBuffer[messageSize];
   char recvBuffer[messageSize];
   memset(sendBuffer, 0, messageSize);
   memset(recvBuffer, 0, messageSize);
   generate_random_message(sendBuffer, messageSize, time(NULL));
-  int err;
-  double sendTime, recvTime, latency;
 
-  sendTime = MPI_Wtime();
-  err = MPI_Sendrecv((void *)sendBuffer, messageSize, MPI_CHAR, 1, 1,
-                     (void *)recvBuffer, messageSize, MPI_CHAR, 1, 1,
-                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  recvTime = MPI_Wtime();
-  if (err != MPI_SUCCESS) {
-    char errorString[64];
-    int errorStringLength;
-    MPI_Error_string(err, errorString, &errorStringLength);
-    printf("Send error in root:%s\n", errorString);
-    return err;
+  for (int i = 0; i < cycles; i++) {
+    double sendTime, recvTime, latency;
+
+    sendTime = MPI_Wtime();
+    err = MPI_Sendrecv((void *)sendBuffer, messageSize, MPI_CHAR, 1, 1,
+                       (void *)recvBuffer, messageSize, MPI_CHAR, 1, 1,
+                       MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    recvTime = MPI_Wtime();
+    if (err != MPI_SUCCESS) {
+      char errorString[64];
+      int errorStringLength;
+      MPI_Error_string(err, errorString, &errorStringLength);
+      printf("Send error in root:%s\n", errorString);
+      return err;
+    }
+
+    latency = recvTime - sendTime;
+    latencySum += latency;
+
+    // Print what was read from node i.
+    log_print(verbose, "Root: Read '%s' from node %d.\n", (char *)recvBuffer,
+              1);
   }
 
-  latency = recvTime - sendTime;
-
-  // Print what was read from node i.
-  log_print(verbose, "Root: Read '%s' from node %d.\n", (char *)recvBuffer, 1);
-
-  printf("Half-round-trip latency was %lf ms.\n", latency/2 * 1000);
+  double latency = latencySum / cycles;
+  printf("Half-round-trip latency was %lf ms.\n", latency / 2 * 1000);
 
   return MPI_SUCCESS;
 }
 
 int ping_exchange_node_run(int rank, int messageSize, int verbose) {
+  int err, cycles = 100;
   char recvBuffer[messageSize];
   char sendBuffer[messageSize];
   memset(sendBuffer, 0, messageSize);
   memset(recvBuffer, 0, messageSize);
   generate_random_message(sendBuffer, messageSize, time(NULL) + rank);
-  int err;
-  err = MPI_Sendrecv(sendBuffer, messageSize, MPI_CHAR, 0, 1, recvBuffer, messageSize, MPI_CHAR, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  if (err != MPI_SUCCESS) {
-    printf("Read error in %d\n", rank);
-    return err;
-  }
+  for (int i = 0; i < cycles; i++) {
+    err = MPI_Sendrecv(sendBuffer, messageSize, MPI_CHAR, 0, 1, recvBuffer,
+                       messageSize, MPI_CHAR, 0, 1, MPI_COMM_WORLD,
+                       MPI_STATUS_IGNORE);
+    if (err != MPI_SUCCESS) {
+      printf("Read error in %d\n", rank);
+      return err;
+    }
 
-  // print what was read from root
-  log_print(verbose, "Node %d: Read '%s' from root.\n", rank, (char *)recvBuffer);
+    // print what was read from root
+    log_print(verbose, "Node %d: Read '%s' from root.\n", rank,
+              (char *)recvBuffer);
+  }
 
   return MPI_SUCCESS;
 }
--- src/ping-pong.c
+++ src/ping-pong.c
@@ -3,60 +3,71 @@
 
 int pingpong_root_run(int size, int messageSize, int verbose) {
   char buffer[messageSize];
-
-  // use sentTime/recvTime array for multiple nodes
-  double sendTime, recvTime, latency;
-  int err;
+  int err, cycles = 100;
+  double latencySum = 0;
 
   // Preparing buffer for send
   memset(buffer, 0, messageSize);
   generate_random_message(buffer, messageSize, time(NULL));
-  sendTime = MPI_Wtime();
-  err = MPI_Send((void *)buffer, messageSize, MPI_CHAR, 1, 1, MPI_COMM_WORLD);
-  if (err != MPI_SUCCESS) {
-    printf("Send error in root\n");
-    return err;
+
+  for (int i = 0; i < cycles; i++) {
+    // use sentTime/recvTime array for multiple nodes
+    double sendTime, recvTime, latency;
+    sendTime = MPI_Wtime();
+    err = MPI_Send((void *)buffer, messageSize, MPI_CHAR, 1, 1, MPI_COMM_WORLD);
+    if (err != MPI_SUCCESS) {
+      printf("Send error in root\n");
+      return err;
+    }
+    memset(buffer, 0, messageSize);
+    err = MPI_Recv((void *)buffer, messageSize, MPI_CHAR, 1, 1, MPI_COMM_WORLD,
+                   MPI_STATUS_IGNORE);
+    recvTime = MPI_Wtime();
+    if (err != MPI_SUCCESS) {
+      printf("Read error in root\n");
+      return err;
+    }
+
+    latency = recvTime - sendTime;
+    latencySum += latency;
+
+    // Print what was read from node i.
+    log_print(verbose, "Root: Received from other node : '%s'\n",
+              (char *)buffer);
   }
-  memset(buffer, 0, messageSize);
-  err = MPI_Recv((void *)buffer, messageSize, MPI_CHAR, 1, 1, MPI_COMM_WORLD,
-                 MPI_STATUS_IGNORE);
-  recvTime = MPI_Wtime();
-  if (err != MPI_SUCCESS) {
-    printf("Read error in root\n");
-    return err;
-  }
 
-  latency = recvTime - sendTime;
-
-  // Print what was read from node i.
-  log_print(verbose, "Root: Received from other node : '%s'\n",  (char *)buffer);
-
+  double latency = latencySum / cycles;
   printf("Half-round-trip latency was %lf ms.\n", latency / 2 * 1000);
   return MPI_SUCCESS;
 }
 
 int pingpong_node_run(int rank, int messageSize, int verbose) {
-  char buffer[messageSize];
-  int err;
+  char recvBuffer[messageSize];
+  char sendBuffer[messageSize];
+  int err, cycles = 100;
 
-  memset(buffer, 0, messageSize);
-  err = MPI_Recv((void *)buffer, messageSize, MPI_CHAR, 0, 1, MPI_COMM_WORLD,
-                 MPI_STATUS_IGNORE);
-  if (err != MPI_SUCCESS) {
-    log_print(verbose, "Read error in %d\n", rank);
-    return err;
+  memset(recvBuffer, 0, messageSize);
+  generate_random_message(sendBuffer, messageSize, time(NULL) + rank);
+
+  for (int i = 0; i < cycles; i++) {
+    err = MPI_Recv((void *)recvBuffer, messageSize, MPI_CHAR, 0, 1, MPI_COMM_WORLD,
+                   MPI_STATUS_IGNORE);
+    if (err != MPI_SUCCESS) {
+      log_print(verbose, "Read error in %d\n", rank);
+      return err;
+    }
+
+    // print what was read from root
+    log_print(verbose, "Node %d: Received from root : '%s'\n", rank,
+              (char *)recvBuffer);
+    memset(recvBuffer, 0, messageSize);
+
+    err = MPI_Send((void *)sendBuffer, messageSize, MPI_CHAR, 0, 1, MPI_COMM_WORLD);
+    if (err != MPI_SUCCESS) {
+      printf("Send error in %d\n", rank);
+      return err;
+    }
   }
 
-  // print what was read from root
-  log_print(verbose, "Node %d: Received from root : '%s'\n", rank,
-            (char *)buffer);
-
-  memset(buffer, 0, messageSize);
-  generate_random_message(buffer, messageSize, time(NULL) + rank);
-  err = MPI_Send((void *)buffer, messageSize, MPI_CHAR, 0, 1, MPI_COMM_WORLD);
-  if (err != MPI_SUCCESS) {
-    printf("Send error in %d\n", rank);
-    return err;
-  }
   return MPI_SUCCESS;
 }
