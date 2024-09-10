4ad28b3508e1b6c93c811438e6457b709f70c655
Funken, Leander Mathias Heinrich <lfunken@students.uni-mainz.de>
2024-09-10 15:05:26 +0200

    build

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ module load mpi/OpenMPI
 mpicc code/main.c code/bundle_args.c code/util.c code/ping-pong/ping-pong.c code/ping-exchange/ping-exchange.c code/config.h code/util.h code/ping-pong/ping-pong.h code/ping-exchange/ping-exchange.h -o build/bundle -lm
 
 # each
-mpicc code/ping-pong/main.c code/util.c code/ping-pong/ping-pong.c code/util.h code/ping-pong/ping-pong.h -o build/pingpong -lm
-mpicc code/ping-exchange/main.c code/util.c code/ping-exchange/ping-exchange.c code/util.h code/ping-exchange/ping-exchange.h -o build/ping_exchange -lm
+mpicc code/ping-pong/main.c code/ping-pong/arg_util.c code/util.c code/ping-pong/ping-pong.c code/util.h code/ping-pong/ping-pong.h -o build/pingpong -lm
+mpicc code/ping-exchange/main.c code/ping-exchange/arg_util.c code/util.c code/ping-exchange/ping-exchange.c code/util.h code/ping-exchange/ping-exchange.h -o build/ping_exchange -lm
 
 # tests
 mpicc code/ping-pong/tests.c code/util.c code/ping-pong/ping-pong.c code/util.h code/ping-pong/ping-pong.h -o build/pingpong_tests -lm
