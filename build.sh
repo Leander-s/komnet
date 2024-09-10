95c9c90efa37c17cf85f42c008f544ae283ce832
Funken, Leander Mathias Heinrich <lfunken@students.uni-mainz.de>
2024-09-10 14:40:12 +0200

    Revert "refactor arg handling"
    This reverts commit 98d9a8706e99cba12a3d995bd8d88ffd814217b9.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ fi
 module load mpi/OpenMPI
 
 # all
-mpicc code/main.c code/bundle_args.c code/util.c code/ping-pong/ping-pong.c code/ping-exchange/ping-exchange.c code/config.h code/util.h code/ping-pong/ping-pong.h code/ping-exchange/ping-exchange.h -o build/bundle -lm
+mpicc code/main.c code/util.c code/ping-pong/ping-pong.c code/ping-exchange/ping-exchange.c code/util.h code/ping-pong/ping-pong.h code/ping-exchange/ping-exchange.h -o build/bundle -lm
 
 # each
 mpicc code/ping-pong/main.c code/util.c code/ping-pong/ping-pong.c code/util.h code/ping-pong/ping-pong.h -o build/pingpong -lm
--- code/util.h
+++ code/util.h
@@ -1,4 +1,9 @@
-#include "config.h"
+#include <errno.h>
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
 
 int string_to_int(char *str);
 void generate_random_message(char *buffer, int bufferSize, int seed);
