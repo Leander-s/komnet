bcdfb01909dfb8d7b02ddf4dca0fb0657b9c1825
Leander <lfunken@students.uni-mainz.de>
2024-09-11 12:19:07 +0200

    tasks per node

--- run_tests.sh
+++ run_tests.sh
@@ -12,9 +12,9 @@ rm ./results_*/*
 
 export OMPI_MCA_btl=^openib,ofi
 # infiniband
-srun --partition $part -N 2 -n 2 ./build/ping_exchange_tests > ./results_infiniband/ib_pe_inter.out &
+srun --partition $part -N 2 -n 2 --ntasks-per-node=1 ./build/ping_exchange_tests > ./results_infiniband/ib_pe_inter.out &
 srun --partition $part -N 1 -n 2 ./build/ping_exchange_tests > ./results_infiniband/ib_pe_intra.out &
-srun --partition $part -N 2 -n 2 ./build/pingpong_tests > ./results_infiniband/ib_pp_inter.out &
+srun --partition $part -N 2 -n 2 --ntasks-per-node=1 ./build/pingpong_tests > ./results_infiniband/ib_pp_inter.out &
 srun --partition $part -N 1 -n 2 ./build/pingpong_tests > ./results_infiniband/ib_pp_intra.out &
 
 export OMPI_MCA_btl=tcp,self
@@ -22,21 +22,21 @@ export OMPI_MCA_btl_sndbuf=2000000
 export OMPI_MCA_btl_rcvbuf=2000000
 
 # task 9.2
-srun --partition $part -N 2 -n 2 ./build/ping_exchange_tests > ./results_92/pe_inter.out &
+srun --partition $part -N 2 -n 2 --ntasks-per-node=1 ./build/ping_exchange_tests > ./results_92/pe_inter.out &
 srun --partition $part -N 1 -n 2 ./build/ping_exchange_tests > ./results_92/pe_intra.out &
-srun --partition $part -N 2 -n 2 ./build/pingpong_tests > ./results_92/pp_inter.out &
+srun --partition $part -N 2 -n 2 --ntasks-per-node=1 ./build/pingpong_tests > ./results_92/pp_inter.out &
 srun --partition $part -N 1 -n 2 ./build/pingpong_tests > ./results_92/pp_intra.out &
 
 # task 9.3
-srun --partition $part -N 2 -n 2 numactl --cpunodebind=0 ./build/ping_exchange_tests > ./results_93/pe_inter_0.out & 
+srun --partition $part -N 2 -n 2 numactl --cpunodebind=0 --ntasks-per-node=1 ./build/ping_exchange_tests > ./results_93/pe_inter_0.out & 
 srun --partition $part -N 1 -n 2 numactl --cpunodebind=0 ./build/ping_exchange_tests > ./results_93/pe_intra_0.out &
-srun --partition $part -N 2 -n 2 numactl --cpunodebind=7 ./build/ping_exchange_tests > ./results_93/pe_inter_7.out &
+srun --partition $part -N 2 -n 2 numactl --cpunodebind=7 --ntasks-per-node=1 ./build/ping_exchange_tests > ./results_93/pe_inter_7.out &
 srun --partition $part -N 1 -n 2 numactl --cpunodebind=7 ./build/ping_exchange_tests > ./results_93/pe_intra_7.out &
-srun --partition $part -N 2 -n 2 numactl --cpunodebind=1 ./build/ping_exchange_tests > ./results_93/pe_inter_1.out &
+srun --partition $part -N 2 -n 2 numactl --cpunodebind=1 --ntasks-per-node=1 ./build/ping_exchange_tests > ./results_93/pe_inter_1.out &
 srun --partition $part -N 1 -n 2 numactl --cpunodebind=1 ./build/ping_exchange_tests > ./results_93/pe_intra_1.out &
-srun --partition $part -N 2 -n 2 numactl --cpunodebind=0 ./build/pingpong_tests > ./results_93/pp_inter_0.out &
+srun --partition $part -N 2 -n 2 numactl --cpunodebind=0 --ntasks-per-node=1 ./build/pingpong_tests > ./results_93/pp_inter_0.out &
 srun --partition $part -N 1 -n 2 numactl --cpunodebind=0 ./build/pingpong_tests > ./results_93/pp_intra_0.out &
-srun --partition $part -N 2 -n 2 numactl --cpunodebind=7 ./build/pingpong_tests > ./results_93/pp_inter_7.out &
+srun --partition $part -N 2 -n 2 numactl --cpunodebind=7 --ntasks-per-node=1 ./build/pingpong_tests > ./results_93/pp_inter_7.out &
 srun --partition $part -N 1 -n 2 numactl --cpunodebind=7 ./build/pingpong_tests > ./results_93/pp_intra_7.out &
-srun --partition $part -N 2 -n 2 numactl --cpunodebind=1 ./build/pingpong_tests > ./results_93/pp_inter_1.out &
+srun --partition $part -N 2 -n 2 numactl --cpunodebind=1 --ntasks-per-node=1 ./build/pingpong_tests > ./results_93/pp_inter_1.out &
 srun --partition $part -N 1 -n 2 numactl --cpunodebind=1 ./build/pingpong_tests > ./results_93/pp_intra_1.out &
