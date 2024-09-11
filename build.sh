aea9c28a67c8f5fac5835f81ca830c0111fd0e43
Funken, Leander Mathias Heinrich <lfunken@students.uni-mainz.de>
2024-09-11 10:44:09 +0200

    refactored

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ fi
 module load mpi/OpenMPI
 
 # all
-mpicc code/main.c code/bundle_args.c code/util.c code/ping-pong/ping-pong.c code/ping-exchange/ping-exchange.c code/bundle_args.h code/config.h code/util.h code/ping-pong/ping-pong.h code/ping-exchange/ping-exchange.h -o build/bundle -lm
+mpicc code/main.c code/bundle_args.c code/util.c code/ping-pong/ping-pong.c code/ping-exchange/ping-exchange.c code/bundle_args.h code/config.h code/util.h code/ping-pong/ping-pong.h code/ping-exchange/ping-exchange.h code/config.h -o build/bundle -lm
 
 # each
-mpicc code/ping-pong/main.c code/ping-pong/arg_util.c code/util.c code/ping-pong/ping-pong.c code/ping-pong/arg_util.h code/util.h code/ping-pong/ping-pong.h -o build/pingpong -lm
-mpicc code/ping-exchange/main.c code/ping-exchange/arg_util.c code/util.c code/ping-exchange/ping-exchange.c code/ping-exchange/arg_util.h code/util.h code/ping-exchange/ping-exchange.h -o build/ping_exchange -lm
+mpicc code/ping-pong/main.c code/ping-pong/arg_util.c code/util.c code/ping-pong/ping-pong.c code/ping-pong/arg_util.h code/util.h code/ping-pong/ping-pong.h code/config.h -o build/pingpong -lm
+mpicc code/ping-exchange/main.c code/ping-exchange/arg_util.c code/util.c code/ping-exchange/ping-exchange.c code/ping-exchange/arg_util.h code/util.h code/ping-exchange/ping-exchange.h code/config.h -o build/ping_exchange -lm
 
 # tests
-mpicc code/ping-pong/tests.c code/util.c code/ping-pong/ping-pong.c code/util.h code/ping-pong/ping-pong.h -o build/pingpong_tests -lm
-mpicc code/ping-exchange/tests.c code/util.c code/ping-exchange/ping-exchange.c code/util.h code/ping-exchange/ping-exchange.h -o build/ping_exchange_tests -lm
+mpicc code/ping-pong/tests.c code/util.c code/ping-pong/ping-pong.c code/util.h code/ping-pong/ping-pong.h code/config.h -o build/pingpong_tests -lm
+mpicc code/ping-exchange/tests.c code/util.c code/ping-exchange/ping-exchange.c code/util.h code/ping-exchange/ping-exchange.h code/config.h -o build/ping_exchange_tests -lm
