446007e9d09d023ebf6c168e2eeccedf144e253b
Funken, Leander Mathias Heinrich <lfunken@students.uni-mainz.de>
2024-09-19 13:46:49 +0200

    scripts updated

--- run_ping_exchange.sh
+++ run_ping_exchange.sh
@@ -2,15 +2,19 @@
 
 SIZE=20
 CYCLES=5
+PART=largemem
 
-while getopts ":s:c:" flag
+while getopts ":s:c:p:" flag
 do
 	case "${flag}" in
 		s) SIZE=$OPTARG;;
 		c) CYCLES=$OPTARG;;
+        p) PART=$OPTARG;;
 		?) echo "Invalid argument: ${OPTARG}.";exit1;;
 	esac
 done
 
+echo "Running on ${PART}"
+
 export OMPI_MCA_btl=tcp,self
-srun --partition largemem -N 2 -n 2 ./build/ping_exchange -s $SIZE -c $CYCLES
+srun --partition $PART -N 2 -n 2 ./build/ping_exchange -s $SIZE -c $CYCLES
